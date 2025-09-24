95a4e4f3604718b3ffaa2943a13302538845f583
Магжан Турмагамбетов <magzhan2002kz@gmail.com>
2025-09-24 23:26:40 +0300

    added m1 homework flask

--- module_01_flask/homework/app.py
+++ module_01_flask/homework/app.py
@@ -1,69 +1,62 @@
+import os
+import random
+import re
 from datetime import datetime, timedelta
 from flask import Flask
-import random
-import os
-import re
 
 app = Flask(__name__)
 
-CARS = ["Chevrolet", "Renault", "Ford", "Lada"]
+cars = ["Chevrolet", "Renault", "Ford", "Lada"]
 
-CATS = [
-    "корниш-рекс",
-    "русская голубая",
-    "шотландская вислоухая",
-    "мейн-кун",
-    "манчкин"
-]
+cats = ["корниш-рекс", "русская голубая", "шотландская вислоухая",
+        "мейн-кун", "манчкин"]
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-BOOK_FILE = os.path.join(BASE_DIR, 'war_and_peace.txt')
+BOOK_FILE = os.path.join(BASE_DIR, "war_and_peace.txt")
 
 def load_words():
-    with open(BOOK_FILE, encoding='utf-8') as book:
-        text = book.read()
-        words = re.findall(r'\b[А-Яа-яЁёA-Za-z-]+\b', text)
-        return words
+    with open(BOOK_FILE, encoding="utf-8") as book:
+        text = book.read().lower()
 
-WORDS = load_words()
+        words = re.findall(r"[а-яА-Яa-zA-Z]+", text)
+    return words
 
-@app.route('/hello_world')
+words_list = load_words()
+
+counter_visits = 0
+
+@app.route("/")
 def hello_world():
-    return "<h1>Привет, мир!</h1>"
+    return "Привет, мир!"
 
 @app.route("/cars")
-def cars():
-    return ", ".join(CARS)
+def get_cars():
+    return ", ".join(cars)
 
 @app.route("/cats")
-def cats():
-    breed = random.choice(CATS)
-    return f"<h1>{breed}</h1>"
+def get_cats():
+    return random.choice(cats)
 
-@app.route("/time")
-def current_time_page():
-    current_time = datetime.now().strftime("%H:%M:%S")
-    return f"<h1>Точное время: {current_time}</h1>"
+@app.route("/get_time/now")
+def get_time_now():
+    current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    return f"Точное время: {current_time}"
 
-@app.route("/time_future_hour")
-def time_future_hour():
-    now = datetime.now()
-    time_after_hour = now + timedelta(hours=1)
-    current_time_after_hour = time_after_hour.strftime("%H:%M:%S")
-    return f"<h1>Точное время через час будет {current_time_after_hour}</h1>"
+@app.route("/get_time/future")
+def get_time_future():
+    current_time_after_hour = (datetime.now() + timedelta(hours=1)).strftime("%Y-%m-%d %H:%M:%S")
+    return f"Точное время через час будет {current_time_after_hour}"
 
 @app.route("/get_random_word")
 def get_random_word():
-    word = random.choice(WORDS)
-    return f"<h1>{word}</h1>"
+    word = random.choice(words_list)
+    return word
 
 @app.route("/counter")
 def counter():
-    counter.visits += 1
-    return f"<h1>Страница открывалась {counter.visits} раз(а)</h1>"
+    global counter_visits
+    counter_visits += 1
+    return f"Страница открывалась {counter_visits} раз(а)."
 
-counter.visits = 0
-
-
-if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5555)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5555, debug=True)
