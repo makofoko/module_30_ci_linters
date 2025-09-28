44140da55bfce618b9c24fd7a00d77754366fa36
Магжан Турмагамбетов <magzhan2002kz@gmail.com>
2025-09-29 01:30:24 +0300

    added 2m homework

--- module_02_linux/homework/hw4/hello_word_with_name.py
+++ module_02_linux/homework/hw4/hello_word_with_name.py
@@ -1,21 +1,32 @@
-"""
-Реализуйте endpoint /hello-world/<имя>, который возвращает строку «Привет, <имя>. Хорошей пятницы!».
-Вместо хорошей пятницы endpoint должен уметь желать хорошего дня недели в целом, на русском языке.
-
-Пример запроса, сделанного в субботу:
-
-/hello-world/Саша  →  Привет, Саша. Хорошей субботы!
-"""
-
 from flask import Flask
+from datetime import datetime
+import sys
 
 app = Flask(__name__)
 
+weekdays = (
+    "понедельника",
+    "вторника",
+    "среды",
+    "четверга",
+    "пятницы",
+    "субботы",
+    "воскресенья",
+)
 
-@app.route('/hello-world/...')
-def hello_world():
-    ...
+@app.route("/hello-world/<name>")
+def hello_world(name: str) -> str:
+    """
+    Endpoint, который возвращает приветствие с учётом текущего дня недели.
+    """
+    weekday_index = datetime.today().weekday()
+    weekday_name = weekdays[weekday_index]
+    return f"Привет, {name}. Хорошего {weekday_name}!"
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
+    print("tuple:", sys.getsizeof(weekdays))
+    print("list:", sys.getsizeof(list(weekdays)))
+    print("dict:", sys.getsizeof({i: day for i, day in enumerate(weekdays)}))
+
     app.run(debug=True)
