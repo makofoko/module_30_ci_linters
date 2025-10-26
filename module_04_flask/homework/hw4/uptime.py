20065a6820fbd5ea8ddb5a02560ad778ebaa7f9c
Magzhan <magzhan2002kz@gmail.com>
2025-10-26 14:17:46 +0500

    added 4m homework

--- module_04_flask/homework/hw4/uptime.py
+++ module_04_flask/homework/hw4/uptime.py
@@ -1,30 +1,15 @@
-class ThresholdValidator:
-    """Класс‑валидатор: можно использовать как объект"""
-    def __init__(self, min_value: int = 1):
-        self.min_value = min_value
+import subprocess
+from flask import Flask
 
-    def __call__(self, value: str) -> int:
-        try:
-            num = int(value)
-            if num < self.min_value:
-                raise ValueError(f"Value must be >= {self.min_value}")
-            return num
-        except Exception:
-            raise ValueError("Invalid threshold value")
-
-from flask import request
+app = Flask(__name__)
 
 @app.route("/uptime", methods=['GET'])
 def uptime() -> str:
     try:
-        threshold_param = request.args.get("threshold", "1")
-
-        threshold = validate_threshold(threshold_param) 
-        validator = ThresholdValidator(1)
-        threshold = validator(threshold_param)
         result = subprocess.check_output(["uptime"], text=True).strip()
-        return f"Current uptime is {result}, threshold={threshold}"
-    except ValueError as ve:
-        return f"Validation error: {ve}", 400
+        return f"Current uptime is {result}"
     except Exception as e:
         return f"Error retrieving uptime: {e}", 500
+
+if __name__ == '__main__':
+    app.run(debug=True)
