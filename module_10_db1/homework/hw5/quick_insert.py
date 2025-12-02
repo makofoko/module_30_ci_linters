459383342ffa5f49cd7a6dca0dfbce5ad93ae489
Магжан Турмагамбетов <magzhan2002kz@gmail.com>
2025-12-03 01:40:22 +0300

    added 10m homework

--- module_10_db1/homework/hw5/quick_insert.py
+++ module_10_db1/homework/hw5/quick_insert.py
@@ -2,18 +2,23 @@ from typing import Union, List
 
 Number = Union[int, float, complex]
 
-
 def find_insert_position(array: List[Number], number: Number) -> int:
-    ...
-
+    left, right = 0, len(array)
+    while left < right:
+        mid = (left + right) // 2
+        if array[mid] < number:
+            left = mid + 1
+        else:
+            right = mid
+    return left
 
 if __name__ == '__main__':
     A: List[Number] = [1, 2, 3, 3, 3, 5]
     x: Number = 4
     insert_position: int = find_insert_position(A, x)
+    print("Позиция для вставки:", insert_position)
     assert insert_position == 5
 
-    A: List[Number] = [1, 2, 3, 3, 3, 5]
-    x: Number = 4
     A.insert(insert_position, x)
+    print("Массив после вставки:", A)
     assert A == sorted(A)
