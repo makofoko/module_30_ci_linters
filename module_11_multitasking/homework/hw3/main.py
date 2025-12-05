867ab039e108260f0edb4c299730ffcc41708f49
Магжан Турмагамбетов <magzhan2002kz@gmail.com>
2025-12-05 15:04:10 +0300

    added 11m homework fixed

--- module_11_multitasking/homework/hw3/main.py
+++ module_11_multitasking/homework/hw3/main.py
@@ -3,30 +3,33 @@ import random
 import threading
 import time
 
-TOTAL_TICKETS = 100     
-AVAILABLE_TICKETS = 10    
+TOTAL_TICKETS = 100
+AVAILABLE_TICKETS = 10
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 class Director(threading.Thread):
     def __init__(self, semaphore: threading.Semaphore):
-        super().__init__()
+        super(Director, self).__init__()
         self.lock = semaphore
         logger.info('Director started work')
 
     def run(self):
         global TOTAL_TICKETS, AVAILABLE_TICKETS
-        while TOTAL_TICKETS > 0:
+        while TOTAL_TICKETS:
             if AVAILABLE_TICKETS < 4:
                 with self.lock:
-                    tickets_to_print = min(10 - AVAILABLE_TICKETS, TOTAL_TICKETS)
+                    tickets_to_print = 10 - (AVAILABLE_TICKETS % 10)
+                    if tickets_to_print > TOTAL_TICKETS:
+                        tickets_to_print = TOTAL_TICKETS
                     AVAILABLE_TICKETS += tickets_to_print
                     TOTAL_TICKETS -= tickets_to_print
-                    logger.info(f'Director put {tickets_to_print} new tickets (available={AVAILABLE_TICKETS}, total={TOTAL_TICKETS})')
-        logger.info('Director stops work, no more tickets left.')
+                    logger.info(f'Director put {tickets_to_print} new tickets')
+        logger.info('Director stops work, not more tickets left.')
 
 class Seller(threading.Thread):
+
     def __init__(self, semaphore: threading.Semaphore):
         super().__init__()
         self.sem = semaphore
@@ -34,27 +37,25 @@ class Seller(threading.Thread):
         logger.info(f'Seller {self.name} started work')
 
     def run(self):
-        global AVAILABLE_TICKETS
+        global TOTAL_TICKETS
         while True:
             self.random_sleep()
             with self.sem:
-                if AVAILABLE_TICKETS <= 0 and TOTAL_TICKETS <= 0:
+                if TOTAL_TICKETS <= 0:
                     break
-                if AVAILABLE_TICKETS > 0:
-                    self.tickets_sold += 1
-                    AVAILABLE_TICKETS -= 1
-                    logger.info(f'{self.name} sold one; available={AVAILABLE_TICKETS}, total={TOTAL_TICKETS}')
-        logger.info(f'Seller {self.name} finished, sold {self.tickets_sold} tickets')
+                self.tickets_sold += 1
+                TOTAL_TICKETS -= 1
+                logger.info(f'{self.name} sold one; {TOTAL_TICKETS} left')
+        logger.info(f'Seller {self.name} sold {self.tickets_sold} tickets')
 
     def random_sleep(self):
-        time.sleep(random.uniform(0, 0.5))
+        time.sleep(random.randint(0,1))
 
 def main():
     semaphore = threading.Semaphore()
-    director = Director(semaphore)
+    director = Director(semaphore=semaphore)
     director.start()
-
-    sellers = []
+    sellers = [director]
     for _ in range(4):
         seller = Seller(semaphore)
         seller.start()
@@ -62,7 +63,7 @@ def main():
 
     for seller in sellers:
         seller.join()
-    director.join()
+
 
 if __name__ == '__main__':
     main()
