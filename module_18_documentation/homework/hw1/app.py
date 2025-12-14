be6e33da925b6f5f92823a860cedfa57cf6fef30
magzh <magzhan2002@gmail.com>
2025-12-15 01:27:50 +0500

    added 18m homework fixed.

--- module_18_documentation/homework/hw1/app.py
+++ module_18_documentation/homework/hw1/app.py
@@ -1,32 +1,60 @@
 from flask import Flask, request, jsonify
 from flasgger import Swagger
 from flasgger.utils import swag_from
-from models import db
-from routes import book_ns, author_ns
-from flask_restx import Api
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///library.db"
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-
-db.init_app(app)
-
-swagger = Swagger(app, template_file='authors.json')
-
-api = Api(app)
-api.add_namespace(book_ns, path="/api/books")
-api.add_namespace(author_ns, path="/api/authors")
+swagger = Swagger(app)
 
 @app.route('/api/books/<int:id>', methods=['GET'])
 @swag_from('books.yml')
 def get_book(id):
+    """Получить книгу по ID"""
     return jsonify({
         "id": id,
         "title": f"Book {id}",
-        "author": "Author Name"
+        "author_id": 1
     })
 
+@app.route('/api/books/', methods=['POST'])
+@swag_from('books.yml')
+def create_book():
+    """Создать книгу"""
+    data = request.json
+    return jsonify({
+        "id": 1,
+        "title": data.get("title"),
+        "author_id": data.get("author_id")
+    }), 201
+
+@app.route('/api/authors/', methods=['POST'])
+@swag_from('authors.json')
+def create_author():
+    """Создать автора"""
+    data = request.json
+    return jsonify({
+        "id": 1,
+        "first_name": data.get("first_name"),
+        "last_name": data.get("last_name"),
+        "middle_name": data.get("middle_name")
+    }), 201
+
+@app.route('/api/authors/<int:id>', methods=['GET'])
+@swag_from('authors.json')
+def get_author(id):
+    """Получить автора по ID"""
+    return jsonify({
+        "id": id,
+        "first_name": "Абай",
+        "last_name": "Кунанбаев",
+        "middle_name": "Ибраһим",
+        "books": [{"id": 1, "title": "Book 1"}]
+    })
+
+@app.route('/api/authors/<int:id>', methods=['DELETE'])
+@swag_from('authors.json')
+def delete_author(id):
+    """Удалить автора по ID"""
+    return jsonify({"message": "Author and all books deleted"}), 200
+
 if __name__ == "__main__":
-    with app.app_context():
-        db.create_all()
     app.run(debug=True)
