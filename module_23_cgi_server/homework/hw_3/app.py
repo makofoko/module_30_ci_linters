2e454ad28c166fbf2ca6874a1f0e408ad5f07d7b
Magzhan Turmagambetov <magzhan2002kz@gmail.com>
2026-06-24 22:12:26 +0500

    added 29m homework

--- module_23_cgi_server/homework/hw_3/app.py
+++
@@ -1,9 +0,0 @@
-import time
-from flask import Flask, jsonify
-
-app = Flask(__name__)
-
-@app.route('/long_task')
-def long_task():
-    time.sleep(300)  # имитация долгой задачи (5 минут)
-    return jsonify(message='We did it!')
