6e5c5c5cd620e75f6f491ff6b547e1b362c1ae99
Magzhan Turmagambetov <magzhan2002kz@gmail.com>
2026-07-01 00:48:21 +0500

    added 30m homework fixed

--- module_30_ci_linters/homework/hw1/database.py
+++ module_30_ci_linters/homework/hw1/database.py
@@ -1,4 +1,5 @@
-from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
+from sqlalchemy.ext.asyncio import (AsyncSession, async_sessionmaker,
+                                    create_async_engine)
 from sqlalchemy.orm import declarative_base
 
 DATABASE_URL = "sqlite+aiosqlite:///./recipes.db"
--- module_30_ci_linters/materials/gitlab_ci/tests/test_application.py
+++
@@ -1,6 +0,0 @@
-from src import main  # type: ignore
-
-
-def test_main_runs():
-    result = main.run()
-    assert result is True
