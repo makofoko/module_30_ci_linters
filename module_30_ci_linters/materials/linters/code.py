ec88ca061470634a30fce771966d3c5bcd0bd79b
Magzhan Turmagambetov <magzhan2002kz@gmail.com>
2026-07-01 00:45:57 +0500

    added 30m homework fixed

--- module_30_ci_linters/materials/linters/code.py
+++ module_30_ci_linters/materials/linters/code.py
@@ -1,56 +1,31 @@
 """Module with some typical mistakes. They aimed to be find by linters."""
 
-import multiprocessing
-import sys
-import tempfile
 from typing import Optional
-
-import flask
-from third_party import (
-    lib1,
-    lib2,
-    lib3,
-    lib4,
-    lib5,
-    lib6,
-    lib7,
-    lib8,
-    lib9,
-    lib10,
-    lib11,
-    lib12,
-    lib13,
-    lib14,
-    lib15,
-)
 
 
 class BadClass:
-    value: str = 42
+    value: int = 42
 
-    def get_value(self) -> "BadClass":
+    def get_value(self) -> str:
         return "some_other_value"
 
-    def compute_something(self) -> None:
+    def compute_something(self) -> bool:
         if self.value == 42:
             return True
-        else:
-            return False
+        return False
 
-    def it_will_fail(self):
-        return self.other_value
+    def it_will_fail(self) -> str:
+        other_value = "safe_value"
+        return other_value
 
 
-def viking_cafe_order(spam: None, beans: str, eggs: Optional[str] = None) -> str:
+def viking_cafe_order(spam: str, beans: str, eggs: Optional[str] = None) -> str:
     del beans, eggs
     return spam + spam + spam
 
 
-import tempfile
-
-
-def compute_other_thing():
+def compute_other_thing() -> None:
     try:
         1 / 0
-    except:
+    except ZeroDivisionError:
         print("oops")
